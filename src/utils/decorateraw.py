447cd24aade5e265624d765c0cb8bca0046bfc4a
tome.huang <tome.huang@samsung.com>
2016-10-28 18:39:09 +0800

    improve script for generating symbols dict

--- src/utils/decorateraw.py
+++ src/utils/decorateraw.py
@@ -1,6 +1,10 @@
 # coding = utf-8
 # decorate raw file which was fetched by fetchpage.py
 
+import json
+import os
+
+
 ANCHOR_NAME_START_B = b'<h1>'
 ANCHOR_NAME_END_B = b'</h1>'
 ANCHOR_EXPLAIN_START_1_B = b'<div class="bookvson2">'
@@ -55,14 +59,72 @@ def decorate(bcontent):
 
     return [byteName.decode().strip(), byteExplain.decode().strip(), byteTranslate.decode().strip(), byteComment.decode().strip()]
 
-def convertToJson(l):
-    pass
+def converContentListtToDict(contentList, index=0):
+    if contentList is None:
+        return None
 
-def decorateFromFolder():
-    pass
+    d = {}
+    d["index"] = index
+    d["name"] = contentList[0]
+    d["explain"] = contentList[1]
+    d["explaintranslate"] = {}
+    d["explaintranslate"]["CN"] = contentList[2]
+    d["comment"] = contentList[3]
+
+    #jsonStr = json.dumps(d, ensure_ascii=False)
+
+    return d
+
+def loadJsonFromFolder(folder, ext='.raw'):
+    l = []
+
+    for root, dirs, files in os.walk(folder):
+        for file in files:
+            if file.endswith(ext):
+                filename = os.path.basename(file)
+                nameComp = filename.split('.')
+                name = nameComp[0]
+                index = int(name)
+
+                path = os.path.join(root, file)
+                print('reading content from file ' + path)
+
+                file = open(path, 'rb')
+                bcontent = file.read()
+                file.close()
+
+                if bcontent is None:
+                    continue
+
+                contentList = decorate(bcontent)
+                if contentList is None:
+                    continue
+
+                itemDict = converContentListtToDict(contentList, index)
+                l.append(itemDict)
+
+    l = sorted(l, key = lambda item: item['index'])
+    jsonRoot = {}
+    jsonRoot['symbolsdict'] = l
+
+    jsonStr = json.dumps(jsonRoot, ensure_ascii=False)
+    #jsonStr = jsonStr.replace('\\n', '\n')
+    #print(jsonStr)
+    return jsonStr
+
 
 if __name__ == '__main__':
-    f = open('0.raw', 'rb')
-    bcontent = f.read()
-    l = decorate(bcontent)
-    print(l)
+    jsonStr = loadJsonFromFolder('.')
+
+    file = open('symboldict', 'wb')
+    file.write(jsonStr.encode())
+    file.close()
+    print('write to file succeed')
+
+    file = open('symboldict', 'rb')
+    content = file.read()
+
+    jsonDict = json.loads(content.decode(), encoding='utf-8')
+    file.close()
+    print(jsonDict)
+
